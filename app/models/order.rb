4b510848008814b336381b372f521da9f9972374
chelsea barocio <barocio.chelsea@gmail.com>
2017-07-26 18:49:43 -0700

    syntax error

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
     has_many :order_items
-    has_many :products, through: order_items 
+    has_many :products, through: :order_items 
 
     def add_product(product_id)
     product = Product.find(product_id)
