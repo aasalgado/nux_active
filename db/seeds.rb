4deb854caa2643cfd27ac364db7da290435b43ab
chelsea barocio <barocio.chelsea@gmail.com>
2017-07-28 09:42:58 -0700

    seeds file- destroy orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+OrderItem.destroy_all
+Order.destroy_all
 User.destroy_all
-
 Product.destroy_all
 
 
